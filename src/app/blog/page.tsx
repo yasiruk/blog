8e914b629cb79facbe996eb08d0c53f7682a114b
Yasiru Kassapa <yasiru@Yasirus-MacBook-Pro.local>
2025-08-16 20:44:05 +0200

    Redesign site and add first blog post.

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DefaultExportComponent = () => {
+  return (
+    <div>
+      <h1>Welcome to the Next.js Page</h1>
+      <p>This is a default export component.</p>
+    </div>
+  );
+};
+
+export default DefaultExportComponent;
---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <div className="grow">
+      <div>
+        <h2 className="title">Recent essays</h2>
+        <ul className="pl-3">
+          <li>
+            <h3>
+              <span>[September 25, 2023]</span> Understanding React Hooks
+            </h3>
+          </li>
+          <li>
+            <h3>Introduction to Next.js</h3>
+            <p>Published on: October 5, 2023</p>
+          </li>
+          <li>
+            <h3>Advanced TypeScript Tips</h3>
+            <p>Published on: October 15, 2023</p>
+          </li>
+        </ul>
+        cla
+      </div>
+    </div>
+  );
+}
